adf9cd521bd130f62a5818783b74f6c0ce005e88
Nguekeu Divin <39398576+divinjordan@users.noreply.github.com>
2020-11-11 15:23:14 +0100

    Add files via upload

---
+++ src/components/shared/Btn.vue
@@ -0,0 +1,35 @@
+<template>
+<button :class="classes">
+    <span class="flex">
+        <Icon v-if="icon" :name="icon" class="mr-2" size="5" stroke="6"/> 
+        <span>
+            {{ label }}
+        </span>
+    </span>
+</button>
+</template>
+
+<script>
+import Icon from '@/components/icons/Icon'
+const defaultStyle = "py-2 px-6 rounded-md text-sm font-medium"
+export default {
+    props: ['class','icon','label','color','outline'],
+    computed: {
+       classes (){  
+           let color;
+           // if button is outline
+           if(this.$props.outline){
+               color = "bg-white border border-green-500 text-green-500"
+           }else{
+               // if the button is not outline
+               color = "bg-green-500 text-white"
+           }
+           return `${color} ${defaultStyle} ${this.$props.class}`
+       } 
+    },
+    components:{
+        Icon
+    }
+}
+
+</script>
---
+++ src/components/shared/BtnIcon.vue
@@ -0,0 +1,20 @@
+<template>
+ <button class="flex item-center p-2 rounded-md border border-green-500 btn-icon hover:bg-green-500 hover:text-white">
+    <icon :name="name" :size="size" :stroke="stroke" class="btn-icon-icon text-green-500 hover:text-white"/>
+</button>
+</template>
+
+<script>
+import Icon from '@/components/icons/Icon'
+export default {
+    props: ['name','size','stroke'],
+    components:{
+        Icon
+    }
+}
+</script>
+<style>
+.btn-icon:hover .btn-icon-icon{
+    @apply text-white
+}
+</style>
